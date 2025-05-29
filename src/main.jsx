796be25243fb438e2283d76b99de1ec829ca2ca1
Stanislav Revasevych <revasevicstanislav@gmail.com>
2025-05-29 15:00:54 +0300

    change styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
+import { ConfigProvider } from "antd";
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Router from "./router.jsx";
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Router />
-  </StrictMode>,
+    <ConfigProvider>
+        <Router/>
+    </ConfigProvider>
 )
